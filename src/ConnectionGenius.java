706c47a3b5f16b75ef88ca246f5826bea668f439
vishwaonline99 <vishwadassanayake99>
2021-01-10 22:39:14 +0530

    Remove Speculative generality

--- src/ConnectionGenius.java
+++ src/ConnectionGenius.java
@@ -8,24 +8,4 @@ public class ConnectionGenius {
   public ConnectionGenius(InetAddress ipa) {
     this.ipa = ipa;
   }
-
-  public void fireUpGame() {
-    downloadWebVersion();
-    connectToWebService();
-    awayWeGo();
-  }
-  
-  public void downloadWebVersion(){
-    System.out.println("Getting specialised web version.");
-    System.out.println("Wait a couple of moments");  
-  }
-  
-  public void connectToWebService() {
-    System.out.println("Connecting");
-  }
-  
-  public void awayWeGo(){
-    System.out.println("Ready to play");
-  }
-
 }
